925ad10c8eed667b6eb1f2bbba8da911706ea558
Rashed <jmrashed.13@gmail.com>
2022-10-17 00:45:06 +0600

    How to stringify object with methods

--- test3.js
+++ test3.js
@@ -1,43 +1,12 @@
-const person = [
-  {name : "Rashed", age : 32 },
-  {name : "raisa", age : 3 },
-  {name : "rekha", age : 48 },
-  {name : "Rejina", age : 25 },
-  {name : "rafia", age : 5 }
-]
-
-function extractArray(arr, customValue, name = 0){
-  var nname = name.toString().toLowerCase();
-  let newArr = arr.filter((val)=>{
-    let lower = val.name.toLowerCase();
-    if(lower.startsWith(customValue)){
-      return val;
-    }
-  })
-if(nname == 0){
-  return newArr;
-}else if(nname === "name"){
-  let arrName = newArr.map((value)=>{
-      return value.name;
-  })
-  return arrName;
-  
-}else if(nname === "age"){
-  let arrName = newArr.map((value)=>{
-    return value.age;
-})
-return arrName;
-}else{
-  console.warn("Pass the arguments currectly");
+const person = {
+  name: "rashed",
+  age : 32,
+  fullName : function(){
+    return this.name + this.age;
+  }
 }
-  
-}
-
-let bb = extractArray(person, "ra", "age");
-
-console.log(bb);
+person.fullName = person.fullName.toString();
+const a = JSON.stringify(person);
 
 
-
-
-
+console.log(a);
