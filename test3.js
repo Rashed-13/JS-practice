478210275c84c30f335b7810ba6b923c6c051bb0
Rashed <jmrashed.13@gmail.com>
2022-10-20 04:20:36 +0600

    JS HTML DOM Practice

--- test3.js
+++ test3.js
@@ -1,29 +1,82 @@
-// Js class practice
+// JS HTML DOM Practice
 
-class Car{
-    constructor(name){
-        this.name = name;
+
+function aminata(){
+    let aniElem = document.getElementById("animate");
+    let pos = 0;
+
+    let intvl = setInterval(fram, 10);
+
+    function fram(){
+        if(pos < 350){
+            pos++;
+            aniElem.style.top = pos + "px";
+            aniElem.style.left = pos + "px";
+        }else{
+            console.log(Math.random());
+            clearInterval(intvl);
+        }
     }
-   static showName() {
-        console.log(this.name);
-   }
+}
+// aminata(); 
+
+
+function myfunc(){
+    console.log("Page loaded");
 }
 
-class Car2 extends Car{
-    constructor(name, model){
-        super(name);
-        this.model = model;
-    }
+function alrtMe(){
+    alert("Hello");
+ }
 
-    carDetails(){
-        console.log(this.name + " " + this.model)
-    }
+ function consoleMe(){
+    console.log("Hello");
+ }
 
+ function removeAlert(){
+    document.getElementById("button").removeEventListener("click", alrtMe)
+ }
+
+ document.getElementById("button").addEventListener("click", alrtMe)
+
+ document.getElementById("button").addEventListener("click", consoleMe)
+
+ document.getElementById("button1").addEventListener("click", removeAlert);
+
+
+
+ window.addEventListener('resize', ()=>{
+    console.log(Math.random());
+ })
+
+
+
+ let pText = document.getElementsByClassName("me");
+ for(let i = 0; i < pText.length; i++){
+    pText[i].innerHTML = "Hello Momo";
 }
 
 
+console.log(pText[0].firstChild.nodeValue);
 
-let car3 = new Car2("Audi", "max2022");
+console.log(document.getElementsByClassName("me")[0].innerText);
 
-car3.carDetails();
-Car2.showName()
+console.log(document);
+console.log(document.documentElement);
+
+let p = document.createElement("p");
+let txt = document.createTextNode("Hello Bangladesh");
+p.appendChild(txt);
+p.classList.add("me");
+
+let cont = document.getElementById("allp");
+let allp = cont.getElementsByTagName("p");
+
+let inbefor = document.getElementById("p1");
+
+cont.insertBefore(p, allp[1]);
+
+console.log(allp);
+
+
+
