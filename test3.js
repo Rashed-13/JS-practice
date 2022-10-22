008ad251f0742bbaa0904e2965ec65c4c4c6e87c
Rashed <jmrashed.13@gmail.com>
2022-10-22 23:19:23 +0600

    JS Local storage and Session storage API practice

--- test3.js
+++ test3.js
@@ -1,28 +1,56 @@
-//JS Form API practice
+//JS Local storage API practice
 
-
-function checkValidit(){
-    const val = document.getElementById("input");
-
-    if(val.validity.rangeUnderflow){
-       val.setCustomValidity("tomi sala pagol");
-    }
-
-    if(!val.checkValidity()){
-        val.setCustomValidity("tomi sala oagol");
-     }
-
-    document.getElementById("demo").innerHTML = val.validationMessage;
-
-
-
-
-
-
-
-
-
+function setSession(){
+    localStorage.setItem("name","Rashed");
 }
+
+function getSession(){
+    localStorage.setItem("age","32");
+}
+
+function removeItem(){
+    localStorage.removeItem("name");
+}
+
+function cleareLocalStorage(){
+    localStorage.clear();
+}
+
+function getSessionKey(){
+    localStorage.key(0);
+}
+
+function getTotalSession(){
+    localStorage.length;
+}
+
+//JS session storage API practice
+
+
+function setLocal(){
+    localStorage.setItem("name","Rashed");
+}
+
+function getLocal(){
+    localStorage.setItem("age","32");
+}
+
+function removeItem(){
+    localStorage.removeItem("name");
+}
+
+function cleareLocalStorage(){
+    localStorage.clear();
+}
+
+function getLocalKey(){
+    localStorage.key(0);
+}
+
+function getTotalSession(){
+    localStorage.length;
+}
+
 
 
 
