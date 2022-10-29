e88f63fb09f728ddef58edec882cad89ad539e54
Rashed <jmrashed.13@gmail.com>
2022-10-30 00:36:14 +0600

    MOD manipulation practice add to card  with image title price MODIFIED

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-// MOD manipulation practice add to card  with image title price
+// MOD manipulation practice add to card  with image title price updated
 
 function addToCard(item){
    let cloneProduct = item.parentElement.cloneNode(true);
-   cloneProduct.children[3].remove();
-   cloneProduct.children[3].classList.remove("hide");
-   document.getElementById("card").appendChild(cloneProduct);
 
-   console.log();
+   cloneProduct.getElementsByClassName("addTocadr")[0].remove();
+   cloneProduct.getElementsByClassName("removeFromCard")[0].classList.remove("hide");
+   document.getElementById("card").appendChild(cloneProduct);
 }
 
 
 function removeFromCard(removeIteme){
    removeIteme.parentElement.remove();
 }
+
 
