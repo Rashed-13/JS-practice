33dc2f68804a1bce44fe056935f2ce6488a85652
Rashed <jmrashed.13@gmail.com>
2022-10-31 01:25:52 +0600

    Lesson #7 - DOM Project - To Do App using DOM - Play with DOM

--- app.js
+++ app.js
@@ -1,16 +1,59 @@
-// MOD manipulation practice add to card  with image title price updated
+// MOD manipulation practice Tream member project just using DOM
 
-function addToCard(item){
-   let cloneProduct = item.parentElement.cloneNode(true);
+document.getElementById("btn").addEventListener("click", ()=>{
+   const element = document.createElement("div");
+   element.id = "me";
+   element.className = "teacher-ara add";
+   
+   const singleTeacher = document.createElement("div");
+   singleTeacher.className = "singleTeacher";
+   
+   const techerImage =  document.createElement("img");
+   techerImage.src = "https://t3.ftcdn.net/jpg/02/65/18/30/360_F_265183061_NkulfPZgRxbNg3rvYSNGGwi0iD7qbmOp.jpg";
+   singleTeacher.appendChild(techerImage);
+   
+   const teacherName = document.createElement("h3");
+   teacherName.innerText = "Jasmin";
+   singleTeacher.appendChild(teacherName);
+   
+   const teachergicnation = document.createElement("p");
+   teachergicnation.innerText = "web developer";
+   singleTeacher.appendChild(teachergicnation);
+   
+   const readMoreButton = document.createElement("button");
+   readMoreButton.innerText = "Read more";
+   readMoreButton.className = "read-more-btn";
+   singleTeacher.appendChild(readMoreButton);
+   
 
-   cloneProduct.getElementsByClassName("addTocadr")[0].remove();
-   cloneProduct.getElementsByClassName("removeFromCard")[0].classList.remove("hide");
-   document.getElementById("card").appendChild(cloneProduct);
-}
+   element.appendChild(singleTeacher);
 
 
-function removeFromCard(removeIteme){
-   removeIteme.parentElement.remove();
-}
+   document.getElementById("root").appendChild(element);
+})
+
+
+
+
+
+document.getElementById("heading").addEventListener("copy", function(){
+confirm("Ai beta amar content copy kortecis ken, toi ki chor naki?");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
