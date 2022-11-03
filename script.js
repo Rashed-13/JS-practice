eff04250b1df998d11f12850d29841e1b46c57aa
Rashed <jmrashed.13@gmail.com>
2022-11-03 10:38:58 +0600

    To Do Application multiple delete feature added

--- script.js
+++ script.js
@@ -9,92 +9,156 @@
  // select elements & assign them to variables
  
 
- let from = document.querySelector('form');
- let inputValue = document.querySelector("#new-task");
- let inCompleteTask = document.querySelector(".todo-list ul");
- let completeTask = document.querySelector(".complete-list ul");
+let form = document.querySelector("form");
+let taskText = document.querySelector("#new-task");
+let inCompleteTask = document.querySelector("#items");
+let completeTask = document.querySelector(".complete-list ul");
 
-//  <li class="item">
-//  <input type="checkbox" /><label>Task Name</label>
-// </li>
 
-function createLi(value){
-    let li = document.createElement("li");
-    li.className = "item";
+
+function createTaskLi(taskValue){
+    let taskLi = document.createElement("li");
+    taskLi.className = "item";
 
     let input = document.createElement("input");
     input.type = "checkbox";
 
-    let label = document.createElement("label");
-    label.innerText = value;
+    let inputLabel = document.createElement("label");
+    inputLabel.innerText = taskValue;
 
-    li.appendChild(input);
-    li.appendChild(label);
+    let btn = document.createElement("button");
+    btn.className = "delete done";
+    btn.innerText ="Done";
 
-    return li;
+    taskLi.appendChild(input);
+    taskLi.appendChild(inputLabel);
+
+    taskLi.appendChild(btn);
+
+    inCompleteTask.appendChild(taskLi);
+    taskText.value = "";
+
+    return taskLi;
 }
 
-
- function createTask(event){
+function creatTask(event){
     event.preventDefault();
-    let li = createLi(inputValue.value);
 
-    inCompleteTask.appendChild(li);
-    inputValue.value = "";
-    
-    onChangeeventBind(li, moveToComplete);
- }
+    let newTask = createTaskLi(taskText.value);
+    inCompleteTask.appendChild(newTask);
 
-/* 
-<li class="item">
-    <input type="checkbox">
-    <label>ad</label>
-</li> 
-*/
+    onChangeEventBinding(newTask, moveToComplete);
+}
 
- function moveToComplete(){
+// hiding and showing group delete button
 
-    let li = this.parentNode;
+function buttonShowHide(){
+    let buttonGrantPrent = this.parentNode.parentNode;
+    let selectAllCheckbox = buttonGrantPrent.querySelectorAll("input[type='checkbox']:checked")
+    let length = selectAllCheckbox.length;
+    if(length > 1){
+        document.getElementById("done-all").classList.remove("hide-done-button");
+    }
+    else{
+        document.getElementById("done-all").classList.add("hide-done-button");
+    }
+}
 
-    let input = li.querySelector("input[type = 'checkbox']");
-    input.remove();
+function onChangeEventBinding(newTask, moveToComplet){
+    let completeDoneButton = newTask.querySelector(".done");
+    completeDoneButton.onclick = moveToComplet;
 
+    let completeInput = newTask.querySelector("input[type='checkbox']");
+    completeInput.onchange = buttonShowHide;  
+}
+
+function moveToComplete(){
+    let completeLI = this.parentNode;
+
+    let completeCheckBox = completeLI.querySelector("input[type='checkbox']");
+    completeCheckBox.remove();
+
+    let completeText = completeLI.querySelector("label").innerText;
+
+    let completeLabel = completeLI.querySelector("label");
+    completeLabel.remove();
+
+    completeLI.innerText = completeText;
 
     let button = document.createElement("button");
     button.className = "delete";
     button.innerText = "Delete";
+    completeLI.appendChild(button);
 
-    li.appendChild(button);
+    completeTask.appendChild(completeLI);
 
-    completeTask.appendChild(li);
+    deleteEventBinding(completeLI, deleteTask);
+}
 
-    deleteEventbinding(li, deleteTask);
-
- };
-
-function deleteEventbinding(li, deleteTask){
-    let deleteBtn = li.querySelector(".delete");
-    deleteBtn.onclick = deleteTask;
+function deleteEventBinding(completeLI, deleteTask){
+    let delBtn = completeLI.querySelector(".delete");
+    delBtn.onclick = deleteTask;    
 }
 
 function deleteTask(){
     this.parentNode.remove();
 }
 
-function onChangeeventBind(li, changeeventBind){
-    let checkBox = li.querySelector("input[type='checkbox']");
-    checkBox.onchange = changeeventBind;
- }
-
-
-for(let i = 0; i<inCompleteTask.children.length; i++){
-    onChangeeventBind(inCompleteTask.children[i], moveToComplete);
+//default incompete task event binding
+for(let i=0; i< inCompleteTask.children.length; i++){
+    onChangeEventBinding(inCompleteTask.children[i], moveToComplete);
 }
 
-for(let i = 0; i<completeTask.children.length; i++){
-    deleteEventbinding(completeTask.children[i], deleteTask);
+//default compete task event binding
+for(let i=0; i<completeTask.children.length; i++){
+    deleteEventBinding(completeTask.children[i], deleteTask);
 }
 
 
+form.onsubmit = creatTask;
 
-from.addEventListener('submit', createTask);
+
+
+
+// This function is for moving all task from incoplete task to complete task section
+function groupMoveToComplete(item){
+    let completeLI = item;
+
+    let completeCheckBox = completeLI.querySelector("input[type='checkbox']");
+    completeCheckBox.remove();
+
+    let completeText = completeLI.querySelector("label").innerText;
+
+    let completeLabel = completeLI.querySelector("label");
+    completeLabel.remove();
+
+    completeLI.innerText = completeText;
+
+    let button = document.createElement("button");
+    button.className = "delete";
+    button.innerText = "Delete";
+    completeLI.appendChild(button);
+
+    completeTask.appendChild(completeLI);
+
+    deleteEventBinding(completeLI, deleteTask);
+}
+
+
+/* selectint all checked box and passing 
+into groupMoveToComplete() for moving to complete section grouply by loop */
+
+document.getElementById("done-all").addEventListener('click', function(){
+    let checkBoox = document.querySelector("#items");
+    let allcheckbox = checkBoox.querySelectorAll("input[type='checkbox']:checked");
+
+    for(i=0; i<allcheckbox.length; i++){
+        groupMoveToComplete(allcheckbox[i].parentNode);
+    }
+    // adding hide-done-button class after loop for diging the button again
+    document.getElementById("done-all").classList.add("hide-done-button");
+})
+
+
+
+
