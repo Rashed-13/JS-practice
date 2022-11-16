653cf0fcf6e00bf8a9a269b625c4dcd8286a8c4c
Rashed <jmrashed.13@gmail.com>
2022-11-16 09:23:57 +0600

    DOM menipulation Tab and menu Project

--- js/main.js
+++ js/main.js
@@ -1,99 +1,88 @@
-// DOM menipulation full project
-let milestoneData = JSON.parse(data);
+// DOM menipulation Tab and menu Project
 
 
-function loadMilestone(){
-  let milestone  = document.querySelector(".milestones");
-  milestone.innerHTML = milestoneData.data.map(function(data){
-    return`<div class="milestone border-b" id = "${data._id}">
-    <div class="flex">
-      <div class="checkbox"><input type="checkbox" onclick="markMilestone(this,${data._id})" /></div>
-      <div onclick="openMilestone(this, ${data._id})">
-        <p>
-          ${data.name}
-          <span><i class="fas fa-chevron-down"></i></span>
-        </p>
-      </div>
-    </div>
-    <div class="hidden_panel">
+let tabHeading = document.querySelectorAll(".tab-heading");
+let tabLength = tabHeading.length;
 
-    ${data.modules.map((dat)=>{
-      return `<div class="module border-b">
-                <p>${dat.name}</p>
-              </div>`}).join("")}
-
-    </div>
-  </div>`
-  }).join("");
-}
-
-loadMilestone();
-
-
-function openMilestone(milestoneElement, id){
-  let currentPannel = milestoneElement.parentNode.nextElementSibling
-  
-  let show = document.querySelector(".show");
-  let active = document.querySelector(".active");
-  
-  if(show && !currentPannel.classList.contains('show')){
-    show.classList.remove("show");
-  }
-
-  if(active && !milestoneElement.classList.contains("active")){
-    active.classList.remove("active");
-  }
-  currentPannel.classList.toggle("show");
-  milestoneElement.classList.toggle("active");
-
-  showMilestoneTitleDestription(id);
-}
-
-function showMilestoneTitleDestription(id){
-  let milestoneImage = document.querySelector(".milestoneImage");
-  let title = document.querySelector(".title");
-  let details = document.querySelector(".details");
-
-  milestoneImage.style.opacity = "0";
-  milestoneImage.src = milestoneData.data[id].image;
-  title.innerHTML = milestoneData.data[id].name;
-  milestoneImage.onload = function(){
-  milestoneImage.style.opacity = "1";
-  }
-
-  if(milestoneData.data[id].description){
-    details.innerHTML = milestoneData.data[id].description;
-  }else{
-    details.innerHTML = "There is no description this is a dumy text Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique eveniet cupiditate beatae labore repudiandae laudantium reprehenderit tempore eaque hic totam";
-  }
-}
-
-
-function markMilestone(checkBox, currentMlstDiveId){
-  let milestones = document.querySelector(".milestones");
-  let doneList = document.querySelector(".doneList");
-  let currentMilestoneDive = document.getElementById(currentMlstDiveId);
-
-  if(checkBox.checked){
-    milestones.removeChild(currentMilestoneDive);
-    doneList.appendChild(currentMilestoneDive);
-  }else{
-    doneList.removeChild(currentMilestoneDive);
-    milestones.appendChild(currentMilestoneDive);
-    reArrangeTask(currentMlstDiveId);
-  }
-}
-
-function reArrangeTask(id){
-  let milestones = document.querySelector(".milestones");
-  let allChindren = document.getElementById(id).parentNode.childNodes;
-  let newArrangedMilestone = [];
-
-  allChindren.forEach(val=>newArrangedMilestone.push(val));
-
-  newArrangedMilestone.sort((a, b)=>{
-    return a.id - b.id;
+for(let i = 0; i<tabLength; i++){
+  tabHeading[i].addEventListener("click", function(e){
+    addContent(this);
+    addClass(e);
   })
-  newArrangedMilestone.forEach(va=>milestones.appendChild(va));
+}
+
+
+function addContent(thiss){
+  let mainhead = thiss.innerHTML;
+  let mainCont = thiss.nextElementSibling.innerHTML;
+
+  let contentHeading = document.querySelector(".content-heading");
+  let contentBody = document.querySelector(".content-body");
+
+  contentHeading.innerHTML = mainhead;
+  contentBody.innerHTML = mainCont;
+}
+
+function addClass(e){
+  let activeTab = document.querySelector(".activeTab");
+  let li = e.target.parentNode;
+
+  if(activeTab && !li.classList.contains("activeTab")){
+    activeTab.classList.remove("activeTab");
+  }
+  e.target.parentNode.classList.add("activeTab");
+}
+
+
+
+
+// Menu Project
+
+let menuu = document.querySelector(".nav ul li");
+
+menuu.addEventListener("click", function(){
+  let mainh = this.parentNode.parentNode;
+  if(mainh.clientWidth == "350"){
+    mainh.style.width = "150px";
+  }else{
+    mainh.style.width = "350px";
+  }
+  console.dir(mainh);
+})
+
+
+document.querySelector(".callContainer").addEventListener("click", function(){
+  togleWidth();
+})
+
+
+function insertWidth1000(){
+  document.querySelector(".callContainer").innerText = "width is now 1000px";
+}
+
+function insertWidth300(){
+  document.querySelector(".callContainer").innerText = "width is now 300px";
+}
+
+
+
+function togleWidth(){
+  if(document.querySelector(".callContainer").clientWidth == "300"){
+     document.querySelector(".callContainer").style.width = "1000px";
+     document.querySelector(".callContainer").addEventListener("transitionend", function(){
+      insertWidth1000();
+    })
+  
+  }else{
+    document.querySelector(".callContainer").style.width = "300px";
+    document.querySelector(".callContainer").addEventListener("transitionend", function(){
+    insertWidth300();
+  })
 
 }
+}
+
+document.querySelector(".callContainer").addEventListener("transitionrun", function(event){
+console.log(this.offsetWidth);
+})
+
